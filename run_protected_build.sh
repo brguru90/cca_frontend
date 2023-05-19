79aba75d519578b0ad13cbd747bce6dfd38ecca2
Guruprasad BR <brguru90@gmail.com>
2023-05-19 21:50:47 +0530

    .

--- run_protected_build.sh
+++ run_protected_build.sh
@@ -10,13 +10,14 @@ exit_status=$?
 
 if test $exit_status -eq 0
 then
-    curl -X -I 'POST' \
+    curl -L -X  'POST' \
     'http://127.0.0.1:8000/api/register_build' \
     -H 'accept: application/json' \
     -H 'Content-Type: multipart/form-data' \
     -F "app_id=$APP_ID" \
     -F "app_secret=$APP_SECRET" \
     -F "auth_key=1234"
+    echo ""
 else
     echo "build failed with exit staus - $exit_status"
 fi
